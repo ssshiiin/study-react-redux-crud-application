4fb1a624430522baa8483d83a669abfe3b82aaa5
Takumi SHINGAI <s1f101902433@iniad.org>
2021-06-15 12:25:13 +0900

    create component and fanctional

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
+import React/*, {Component}*/ from 'react';
 
-class App extends Component {
+/*class App extends Component {
   render(){
     const v = "Hi!"
     const d = <h1 className = "foo">{v}</h1>
@@ -11,6 +11,20 @@ class App extends Component {
       </React.Fragment>
     )
   }
+}*/
+
+const Cat = () => {
+  return <div>meow!</div>
+}
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
 }
 
 export default App;
