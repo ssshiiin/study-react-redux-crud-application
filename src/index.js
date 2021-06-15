ba1f9406006df05a251537c9fa0b6b507dd60741
Takumi SHINGAI <s1f101902433@iniad.org>
2021-06-16 00:24:27 +0900

    implement store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+
 import './index.css';
-import App from './App';
+import App from './componets/App';
 import reportWebVitals from './reportWebVitals';
+import reducer from './reducers'
+
+
+const store  = createStore(reducer)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store = {store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +24,8 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
+
